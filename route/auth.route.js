a68ed679057a1aea99203d836dc6786c53e685ca
kingsuk <kingsuk055@gmail.com>
2024-04-21 09:34:05 +0530

    feat - Image optimization

--- route/auth.route.js
+++ route/auth.route.js
@@ -1,31 +1,19 @@
-import express from "express";
-import {
-  signup,
-  login,
-  verifyOtp,
-  forgotPasswordRequest,
-  forgotPassword,
-} from "../controller/auth.controller.js";
-import { verifySchema } from "../middleware/verifySchema.js";
-import {
-  RegisterSchema,
-  LoginSchema,
-  requestSchema,
-  resetSchema,
-} from "../validation/userValodation.js";
-import upload from "../middleware/upload.js";
-const router = express.Router();
+import express from 'express';
+import multer from 'multer';
 
-router.post(
-  "/signup",
-  upload.single("avatar"),
-  verifySchema(RegisterSchema),
-  signup
-);
-router.post("/login", verifySchema(LoginSchema), login);
+import { signup, login, verifyOtp, forgotPasswordRequest, forgotPassword } from '../controller/auth.controller.js';
+import { verifySchema } from '../middleware/verifySchema.js';
+import { RegisterSchema, LoginSchema, requestSchema, resetSchema } from '../validation/userValodation.js';
+
+const router = express.Router();
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+router.post('/signup', upload.single('avatar'), verifySchema(RegisterSchema), signup);
+router.post('/login', verifySchema(LoginSchema), login);
 router
-  .post("/verifyOTP", verifyOtp)
-  .post("/request", verifySchema(requestSchema), forgotPasswordRequest)
-  .post("/reset", verifySchema(resetSchema), forgotPassword);
+	.post('/verifyOTP', verifyOtp)
+	.post('/request', verifySchema(requestSchema), forgotPasswordRequest)
+	.post('/reset', verifySchema(resetSchema), forgotPassword);
 
 export default router;
